62975edbcc2ba873090791e27034c227bdc71a46
Iain Lane <iain@grafana.com>
2025-05-16 10:41:46 +0100

    feat(control-plane): add support for handling multiple events in a single invocation
    Currently we restrict the `scale-up` Lambda to only handle a single
    event at a time. In very busy environments this can prove to be a
    bottleneck: there are calls to GitHub and AWS APIs that happen each
    time, and they can end up taking long enough that we can't process
    job queued events faster than they arrive.
    
    In our environment we are also using a pool, and typically we have
    responded to the alerts generated by this (SQS queue length growing) by
    expanding the size of the pool. This helps because we will more
    frequently find that we don't need to scale up, which allows the lambdas
    to exit a bit earlier, so we can get through the queue faster. But it
    makes the environment much less responsive to changes in usage patterns.
    
    At its core, this Lambda's task is to construct an EC2 `CreateFleet`
    call to create instances, after working out how many are needed. This is
    a job that can be batched. We can take any number of events, calculate
    the diff between our current state and the number of jobs we have,
    capping at the maximum, and then issue a single call.
    
    The thing to be careful about is how to handle partial failures, if EC2
    creates some of the instances we wanted but not all of them. Lambda has
    a configurable function response type which can be set to
    `ReportBatchItemFailures`. In this mode, we return a list of failed
    messages from our handler and those are retried. We can make use of this
    to give back as many events as we failed to process.
    
    Now we're potentially processing multiple events in a single Lambda, one
    thing we should optimise for is not recreating GitHub API clients. We
    need one client for the app itself, which we use to find out
    installation IDs, and then one client for each installation which is
    relevant to the batch of events we are processing. This is done by
    creating a new client the first time we see an event for a given
    installation.
    
    We also remove the same `batch_size = 1` constraint from the `job-retry`
    Lambda and make it configurable instead, using AWS's default of 10 for
    SQS if not configured. This Lambda is used to retry events that
    previously failed. However, instead of reporting failures to be retried,
    here we maintain the pre-existing fault-tolerant behaviour where errors
    are logged but explicitly do not cause message retries, avoiding
    infinite loops from persistent GitHub API issues or malformed events.
    
    Tests are added for all of this.
    
    (cherry picked from commit 9443df80097d10fa228471c3690b42582563c152)

--- lambdas/functions/control-plane/src/lambda.ts
+++ lambdas/functions/control-plane/src/lambda.ts
@@ -1,34 +1,72 @@
 import middy from '@middy/core';
 import { logger, setContext } from '@aws-github-runner/aws-powertools-util';
 import { captureLambdaHandler, tracer } from '@aws-github-runner/aws-powertools-util';
-import { Context, SQSEvent } from 'aws-lambda';
+import { Context, type SQSBatchItemFailure, type SQSBatchResponse, SQSEvent } from 'aws-lambda';
 
 import { PoolEvent, adjust } from './pool/pool';
 import ScaleError from './scale-runners/ScaleError';
 import { scaleDown } from './scale-runners/scale-down';
-import { scaleUp } from './scale-runners/scale-up';
+import { type ActionRequestMessage, type ActionRequestMessageSQS, scaleUp } from './scale-runners/scale-up';
 import { SSMCleanupOptions, cleanSSMTokens } from './scale-runners/ssm-housekeeper';
 import { checkAndRetryJob } from './scale-runners/job-retry';
 
-export async function scaleUpHandler(event: SQSEvent, context: Context): Promise<void> {
+export async function scaleUpHandler(event: SQSEvent, context: Context): Promise<SQSBatchResponse> {
   setContext(context, 'lambda.ts');
   logger.logEventIfEnabled(event);
 
-  if (event.Records.length !== 1) {
-    logger.warn('Event ignored, only one record at the time can be handled, ensure the lambda batch size is set to 1.');
-    return Promise.resolve();
+  // Group the messages by their event source. We're only interested in
+  // `aws:sqs`-originated messages.
+  const groupedEvents = new Map<string, ActionRequestMessageSQS[]>();
+  for (const { body, eventSource, messageId } of event.Records) {
+    const group = groupedEvents.get(eventSource) || [];
+    const payload = JSON.parse(body) as ActionRequestMessage;
+
+    if (group.length === 0) {
+      groupedEvents.set(eventSource, group);
+    }
+
+    groupedEvents.get(eventSource)?.push({
+      ...payload,
+      messageId,
+    });
   }
 
+  for (const [eventSource, messages] of groupedEvents.entries()) {
+    if (eventSource === 'aws:sqs') {
+      continue;
+    }
+
+    logger.warn('Ignoring non-sqs event source', { eventSource, messages });
+  }
+
+  const sqsMessages = groupedEvents.get('aws:sqs') ?? [];
+
+  // Sort messages by their retry count, so that we retry the same messages if
+  // there's a persistent failure. This should cause messages to be dropped
+  // quicker than if we retried in an arbitrary order.
+  sqsMessages.sort((l, r) => {
+    return (l.retryCounter ?? 0) - (r.retryCounter ?? 0);
+  });
+
+  const batchItemFailures: SQSBatchItemFailure[] = [];
+
   try {
-    await scaleUp(event.Records[0].eventSource, JSON.parse(event.Records[0].body));
-    return Promise.resolve();
+    const rejectedMessageIds = await scaleUp(sqsMessages);
+
+    for (const messageId of rejectedMessageIds) {
+      batchItemFailures.push({
+        itemIdentifier: messageId,
+      });
+    }
+
+    return { batchItemFailures };
   } catch (e) {
     if (e instanceof ScaleError) {
-      return Promise.reject(e);
-    } else {
-      logger.warn(`Ignoring error: ${e}`);
-      return Promise.resolve();
+      throw e;
     }
+
+    logger.warn(`Will retry error: ${e}`);
+    return { batchItemFailures };
   }
 }
 
